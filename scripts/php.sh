cba1cef9fdf6c04c1bb3589811d621d7d967c39d
Hiroiku <hiroiku@users.noreply.github.com>
2019-02-14 11:43:21 +0900

    Update php.sh

--- scripts/php.sh
+++ scripts/php.sh
@@ -2,7 +2,7 @@
 
 # Install PHP 7.2
 amazon-linux-extras install -y php7.2
-yum install -y php-bcmath php-common php-fpm php-gd php-intl php-json php-mbstring php-mysql php-opcache php-pdo php-xml
+yum install -y php-bcmath php-common php-fpm php-gd php-intl php-json php-mbstring php-opcache php-pdo php-xml
 
 # Enable php7.2-fpm
 systemctl enable php-fpm
