0c88de2f96dabb6f95cd630149c1dfc06ea77fa4
Hiroiku Inoue <mail@hiroiku.me>
2019-03-22 11:16:59 +0900

    Update scripts/php.sh

--- scripts/php.sh
+++ scripts/php.sh
@@ -2,8 +2,13 @@
 
 # Install PHP 7.2
 amazon-linux-extras install -y php7.2
-yum install -y php-bcmath php-common php-fpm php-gd php-intl php-json php-mbstring php-mysql php-opcache php-pdo php-xml
+yum install -y php-bcmath php-common php-fpm php-gd php-intl php-json php-mbstring php-opcache php-pdo php-xml
+yum install -y memcached php-pecl-memcached
 
 # Enable php7.2-fpm
 systemctl enable php-fpm
 systemctl start php-fpm
+
+# Enable memcached
+systemctl enable memcached
+systemctl start memcached
