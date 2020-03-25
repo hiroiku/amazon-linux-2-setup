e3f6505748d8e2912233a67b641662f37a672f9a
Hiroiku <hiroiku@users.noreply.github.com>
2020-03-25 17:15:13 +0900

    Update php.sh

--- scripts/php.sh
+++ scripts/php.sh
@@ -7,10 +7,8 @@ if [ ${EUID:-${UID}} != 0 ]; then
     exit 1
 fi
 
-# Install PHP 7.2
-VERSIONS=(`amazon-linux-extras list | grep -oP "^\s+?.+\s+?\K(php\d+?\.\d+?)" | sort -nr`)
-LATEST=("${VERSIONS[@]:0}")
-amazon-linux-extras install -y $LATEST
+# Install PHP 7.3
+amazon-linux-extras install -y php7.3
 
 yum install -y php-bcmath php-common php-fpm php-gd php-intl php-json php-mbstring php-opcache php-pdo php-xml
 systemctl enable php-fpm
