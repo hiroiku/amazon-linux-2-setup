f2084411c64df52e417a9897c3ee2c724dce2a4d
Hiroiku <hiroiku@users.noreply.github.com>
2021-11-15 15:49:30 +0900

    Update php.sh

--- scripts/php.sh
+++ scripts/php.sh
@@ -8,13 +8,14 @@ if [ ${EUID:-${UID}} != 0 ]; then
 fi
 
 # Install PHP 7.3
-amazon-linux-extras install -y php7.3
+amazon-linux-extras install -y php8.0
 
 yum install -y php-bcmath php-common php-fpm php-gd php-intl php-json php-mbstring php-opcache php-pdo php-xml
 systemctl enable php-fpm
 systemctl start php-fpm
 
 # Enable memcached
-yum install -y memcached php-pecl-memcached
+amazon-linux-extras install -y memcached1.5
+yum install -y php-pecl-memcached
 systemctl enable memcached
 systemctl start memcached
